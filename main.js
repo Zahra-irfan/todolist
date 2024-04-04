abe6cd1ef147661229e7d1483131a1a78705617a
zahrairfan25 <zadssiblings@gmail.com>
2024-04-03 18:19:35 -0700

    Additional functions

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 import inquirer from "inquirer";
 let toDo = [];
 let condition = true;
+// Getting the user to choose how to add items to the to do list;
 let greet = await inquirer.prompt({
     name: "initial",
     message: "Hello, how would you like to add items to your list today?",
     type: "list",
-    choices: ["Input manually", "Quick add", "View Items"]
+    choices: ["Input manually", "Quick add"]
 });
+// If the user chooses the option to add manually;
 if (greet.initial === "Input manually") {
     while (condition) {
         let toDoList = await inquirer.prompt([
@@ -19,27 +21,72 @@ if (greet.initial === "Input manually") {
                 type: "confirm",
                 default: true }
         ]);
+        // Making sure that the user doesn't add an empty item to the list; 
         if (toDoList.tasks.length > 0) {
             toDo.push(toDoList.tasks);
-            console.log(toDo);
+            toDo.forEach((items) => { console.log(items); });
         }
         else {
             console.log("Please enter a task you wish to add to your to do list!");
         }
         ;
+        // Giving the user additional choices to choose wether to exit, delete an item, view the list or continue adding items.
         if (toDoList.confirm === true) {
             let progress = await inquirer.prompt({
                 name: "continue",
                 message: "What would you like to do now?",
                 type: "list",
-                choices: ["View Items", "Continue", "Exit"]
+                choices: ["View Items", "Continue", "Delete", "Exit"]
             });
+            // If the user wishes to view his to do list, printing the list.
             if (progress.continue === "View Items") {
-                console.log(toDo);
+                toDo.forEach((items) => { console.log(items); });
+                // Giving user another choice to let him choose what he wants to do after viewing the list;
+                let choice2 = await inquirer.prompt({
+                    name: "choose",
+                    message: "What would you like to do now?",
+                    type: "list",
+                    choices: ["Continue", "Exit"]
+                });
+                if (choice2.choose === "Exit") {
+                    condition = false;
+                }
+                else {
+                    condition = true;
+                }
+                ;
             }
+            // If the user wishes to continue;
             else if (progress.continue === "Continue") {
                 condition = toDoList.confirm;
             }
+            // If the user wishes to delete an item from the list, let him choose the position of item he wishes to delete.
+            //  Leting the user further choose if he wishes to continue or exit after deleting the desired item from the list,
+            else if (progress.continue === "Delete") {
+                toDo.index;
+                let index = await inquirer.prompt({
+                    name: "number",
+                    message: "Please enter the serial number of item you wish to delete:",
+                    type: "number",
+                });
+                let deletes = index.number - 1;
+                toDo.splice(deletes, 1);
+                toDo.forEach((items) => { console.log(items); });
+                let choice2 = await inquirer.prompt({
+                    name: "choose",
+                    message: "What would you like to do now?",
+                    type: "list",
+                    choices: ["Continue", "Exit"]
+                });
+                if (choice2.choose === "Exit") {
+                    condition = false;
+                }
+                else {
+                    condition = true;
+                }
+                ;
+            }
+            // If the user wsihes to exit the to do list.
             else {
                 condition = false;
             }
@@ -49,6 +96,7 @@ if (greet.initial === "Input manually") {
         }
     }
 }
+// Leting user to quicklly add items to the list by giving him multiple options giving him the same choices as were given in input manually. 
 else if (greet.initial === "Quick add") {
     while (condition) {
         let to_do = await inquirer.prompt([
@@ -62,19 +110,56 @@ else if (greet.initial === "Quick add") {
                 default: true }
         ]);
         toDo.push(to_do.task);
-        console.log(toDo);
+        toDo.forEach((items) => { console.log(items); });
         if (to_do.confirmation === true) {
             let progress = await inquirer.prompt({
                 name: "continue",
                 message: "What would you like to do now?",
                 type: "list",
-                choices: ["View Items", "Continue", "Exit"]
+                choices: ["View Items", "Continue", "Delete", "Exit"]
             });
             if (progress.continue === "View Items") {
-                console.log(toDo);
+                toDo.forEach((items) => { console.log(items); });
+                let choice2 = await inquirer.prompt({
+                    name: "choose",
+                    message: "What would you like to do now?",
+                    type: "list",
+                    choices: ["Continue", "Exit"]
+                });
+                if (choice2.choose === "Exit") {
+                    condition = false;
+                }
+                else {
+                    condition = true;
+                }
+                ;
             }
             else if (progress.continue === "Continue") {
                 condition = to_do.confirmation;
+            }
+            else if (progress.continue === "Delete") {
+                toDo.index;
+                let index = await inquirer.prompt({
+                    name: "number",
+                    message: "Please enter the serial number of item you wish to delete:",
+                    type: "number",
+                });
+                let deletes = index.number - 1;
+                toDo.splice(deletes, 1);
+                toDo.forEach((items) => { console.log(items); });
+                let choice2 = await inquirer.prompt({
+                    name: "choose",
+                    message: "What would you like to do now?",
+                    type: "list",
+                    choices: ["Continue", "Exit"]
+                });
+                if (choice2.choose === "Exit") {
+                    condition = false;
+                }
+                else {
+                    condition = true;
+                }
+                ;
             }
             else {
                 condition = false;
@@ -84,7 +169,4 @@ else if (greet.initial === "Quick add") {
             condition = to_do.confirmation;
         }
     }
-}
-else if (greet.initial === "View Items") {
-    console.log(toDo);
 }
--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@
 import inquirer from "inquirer";
 let toDo:any = [];
 let condition = true;
+// Getting the user to choose how to add items to the to do list;
 let greet= await inquirer.prompt({
   name:"initial",
   message:"Hello, how would you like to add items to your list today?",
   type:"list",
-  choices:["Input manually","Quick add","View Items"]
+  choices:["Input manually","Quick add"]
 })
+// If the user chooses the option to add manually;
 if (greet.initial === "Input manually"){
 while (condition) {
   let toDoList = await inquirer.prompt
@@ -21,23 +23,63 @@ while (condition) {
         type:"confirm",
         default:true}
     ]
-    );if (toDoList.tasks.length>0){
+    );
+    // Making sure that the user doesn't add an empty item to the list; 
+    if (toDoList.tasks.length>0){
     toDo.push(toDoList.tasks);
-    console.log(toDo);} 
+    toDo.forEach((items : string)=>{console.log(items)})} 
     else {console.log("Please enter a task you wish to add to your to do list!")};
+    // Giving the user additional choices to choose wether to exit, delete an item, view the list or continue adding items.
     if (toDoList.confirm===true){
     let progress = await inquirer.prompt({
       name:"continue",
       message:"What would you like to do now?",
       type:"list",
-      choices:["View Items","Continue", "Exit"]
+      choices:["View Items","Continue","Delete", "Exit"]
     })
+    // If the user wishes to view his to do list, printing the list.
     if (progress.continue === "View Items"){
-      console.log(toDo)
-    } else if (progress.continue === "Continue") {condition = toDoList.confirm;}
+      toDo.forEach((items : string)=>{console.log(items)});
+      // Giving user another choice to let him choose what he wants to do after viewing the list;
+      let choice2 = await inquirer.prompt({
+        name:"choose",
+        message:"What would you like to do now?",
+        type:"list",
+        choices:["Continue", "Exit"]
+      })
+      if (choice2.choose === "Exit"){
+      condition = false}
+      else {condition = true};                
+    } 
+    // If the user wishes to continue;
+    else if (progress.continue === "Continue") {condition = toDoList.confirm;}
+    // If the user wishes to delete an item from the list, let him choose the position of item he wishes to delete.
+  //  Leting the user further choose if he wishes to continue or exit after deleting the desired item from the list,
+    else if (progress.continue === "Delete"){
+      toDo.index 
+      let index = await inquirer.prompt({
+        name:"number",
+        message:"Please enter the serial number of item you wish to delete:",
+        type:"number",
+      });
+      let deletes = index.number - 1;
+      toDo.splice(deletes, 1);
+      toDo.forEach((items : string)=>{console.log(items)});
+      let choice2 = await inquirer.prompt({
+        name:"choose",
+        message:"What would you like to do now?",
+        type:"list",
+        choices:["Continue", "Exit"]
+      })
+      if (choice2.choose === "Exit"){
+      condition = false}
+      else {condition = true};
+    }
+    // If the user wsihes to exit the to do list.
    else {condition = false}}   
     else {condition = toDoList.confirm;}      
 }}
+// Leting user to quicklly add items to the list by giving him multiple options giving him the same choices as were given in input manually. 
 else if (greet.initial === "Quick add"){
   while (condition) {
     let to_do = await inquirer.prompt
@@ -54,20 +96,48 @@ else if (greet.initial === "Quick add"){
       ]
       );
       toDo.push(to_do.task);
-      console.log(toDo);
+      toDo.forEach((items : string)=>{console.log(items)})
       if (to_do.confirmation === true){
       let progress = await inquirer.prompt({
         name:"continue",
         message:"What would you like to do now?",
         type:"list",
-        choices:["View Items","Continue", "Exit"]
+        choices:["View Items","Continue","Delete", "Exit"]
       })
       if (progress.continue === "View Items"){
-        console.log(toDo);
+        toDo.forEach((items : string)=>{console.log(items)});
+        let choice2 = await inquirer.prompt({
+          name:"choose",
+          message:"What would you like to do now?",
+          type:"list",
+          choices:["Continue", "Exit"]
+        })
+        if (choice2.choose === "Exit"){
+        condition = false}
+        else {condition = true};
       } else if (progress.continue === "Continue") {condition = to_do.confirmation;}
+      else if (progress.continue === "Delete"){
+        toDo.index 
+        let index = await inquirer.prompt({
+          name:"number",
+          message:"Please enter the serial number of item you wish to delete:",
+          type:"number",
+        });
+        let deletes = index.number - 1;
+        toDo.splice(deletes, 1);
+        toDo.forEach((items : string)=>{console.log(items)});
+        let choice2 = await inquirer.prompt({
+          name:"choose",
+          message:"What would you like to do now?",
+          type:"list",
+          choices:["Continue", "Exit"]
+        })
+        if (choice2.choose === "Exit"){
+        condition = false}
+        else {condition = true};
+      }
     else {condition = false}}
       else {condition = to_do.confirmation;}
   }}
-  else if (greet.initial === "View Items"){
-    console.log(toDo);
-  }                  
+                 
+  
